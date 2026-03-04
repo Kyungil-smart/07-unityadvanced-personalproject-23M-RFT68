072c045d6b259631b3dd832cd21b286d3c62f62b
23M-RFT68 <kobh0221@gmail.com>
2026-03-04 18:01:02 +0900

    [Create] Monster && Reposition Scripts

---
+++ PersonalProject/Assets/Scripts/Monster.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody2D Target;
+
+    private bool isAlive = true;
+
+    private Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isAlive) return;
+
+        Vector2 dirVec = Target.position - rb.position;
+        Vector2 nextVec = dirVec.normalized * Speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
+        rb.linearVelocity = Vector2.zero;
+    }
+
+    private void LateUpdate()
+    {
+        if (!isAlive) return;
+        sr.flipX = Target.position.x < rb.position.x;
+    }
+}
--- PersonalProject/Assets/Scripts/Reposition.cs
+++ PersonalProject/Assets/Scripts/Reposition.cs
@@ -1,16 +1,38 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnTriggerExit2D(Collider2D collision)
     {
+        if (!collision.CompareTag("Area")) return;
         
-    }
+        Vector3 PlayerPos = GameManager.instance.player.transform.position;
+        Vector3 MyPos = transform.position;
+        
+        float diffX = MathF.Abs(PlayerPos.x - MyPos.x);
+        float diffY = MathF.Abs(PlayerPos.y - MyPos.y);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 PlayerDir = GameManager.instance.player.inputVec;
+        float dirX = PlayerDir.x < 0 ? -1 : 1;
+        float dirY = PlayerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+                
+                break;
+        }
     }
 }
