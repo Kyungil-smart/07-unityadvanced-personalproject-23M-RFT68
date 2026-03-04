cea096a463871940d9b5b46a72cc7fe98ffbb7a3
23M-RFT68 <kobh0221@gmail.com>
2026-03-04 20:10:43 +0900

    [Edit] GameManager

--- PersonalProject/Assets/Scripts/GameManager.cs
+++ PersonalProject/Assets/Scripts/GameManager.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public PoolManager poolManager;
     public Player player;
 
     void Awake()
     {
         instance = this;
-        
-        
     }
 }
