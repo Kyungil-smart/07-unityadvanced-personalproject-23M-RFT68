cc5a9810ba518e9be845b73a5a16afd5fc7450de
23M-RFT68 <kobh0221@gmail.com>
2026-03-05 11:10:52 +0900

    [Edit] Monster && GameManager

--- PersonalProject/Assets/Scripts/Monster.cs
+++ PersonalProject/Assets/Scripts/Monster.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,19 +14,24 @@ public class Monster : MonoBehaviour
     private bool isAlive;
 
     private Rigidbody2D rb;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer sr;
-
+    private WaitForFixedUpdate wait;
+    
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     private void FixedUpdate()
     {
-        if (!isAlive) return;
+        if (!isAlive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")) return;
 
         Vector2 dirVec = Target.position - rb.position;
         Vector2 nextVec = dirVec.normalized * Speed * Time.fixedDeltaTime;
@@ -43,6 +49,10 @@ public class Monster : MonoBehaviour
     {
         Target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isAlive = true;
+        coll.enabled = true;
+        rb.simulated = true;
+        sr.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         health = maxHealth;
     }
 
@@ -56,18 +66,33 @@ public class Monster : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet")) return;
+        if (!collision.CompareTag("Bullet") || !isAlive) return;
 
         health -= collision.GetComponent<Bullet>().damage;
-
+        StartCoroutine(Knockback());
+        
         if (health > 0)
         {
-            
+            anim.SetTrigger("Hit");
         }
         else
         {
-            Dead();
+            isAlive = false;
+            coll.enabled = false;
+            rb.simulated = false;
+            sr.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
+    }
+
+    IEnumerator Knockback() // 넉백을 주는 로직
+    {
+        yield return wait;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rb.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
     }
 
     private void Dead()
